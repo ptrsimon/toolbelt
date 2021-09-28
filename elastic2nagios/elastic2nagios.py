853154ea1cee58441ba6f2aa189290177ee4f609
Peter Simon <psimonprs@gmail.com>
2021-09-28 15:55:37 +0200

    Add support for creation of multiple alerts in one request

--- elastic2nagios/elastic2nagios.py
+++ elastic2nagios/elastic2nagios.py
@@ -11,11 +11,9 @@ import config
 
 class List:
     def on_get(self, req, resp):
-        # Load existing alerts
         with open(config.alertfile) as fh:
             alerts = json.loads(fh.read())
 
-        # Check alert data
         required = [ "plugin_output", "service", "status", "hostname", "id", "last_state_change"]
         for i in alerts:
             for j in required:
@@ -28,7 +26,6 @@ class List:
                     resp.media = media
                     return
         
-        # Enrich alerts with Nagios data and count
         for i in alerts:
             i["flags"] = 11
             i["host_alive"] = 1
@@ -44,7 +41,6 @@ class List:
             else:
                 i.pop("count")
         
-        # Construct response
         response = {}
         response["version"] = config.version
         response["running"] = 1
@@ -53,51 +49,23 @@ class List:
         resp.media = response
     
 class Create:
-    def on_post(self, req, resp):
-        # Check required parameters
-        required = [ "plugin_output", "service", "status", "hostname" ]
-        for i in required:
-            if i not in req.media.keys():
-                media = {
-                    "error": i + " is missing",
-                    "status": 400
-                }
-                resp.status = falcon.HTTP_400
-                resp.media = media
-                return
-
-        # Input check
-        forbidden_chars = re.compile('[<>;]')
-        if forbidden_chars.search(req.media["plugin_output"]) != None:
-            media = {
-                "error": "Invalid character in plugin_output",
-                "status": 400
-            }
-            resp.status = falcon.HTTP_400
-            resp.media = media
-            return
-        
-        # Load existing alerts if any
+    def add_alert(self, newalert):
         if os.path.exists(config.alertfile):
             with open(config.alertfile) as fh:
                 alerts = json.loads(fh.read())
         else:
             alerts = []
 
-        # Generate alert ID
         alertid = 0
         for i in alerts:
             if i["id"] > alertid:
                 alertid = i["id"]
         alertid += 1
 
-        # Add some other data to the new alert
-        newalert = req.media
         newalert["id"] = alertid
         newalert["last_state_change"] = int(time.time())
         newalert["count"] = 1
 
-        # Check for duplicates
         duplicate = False
         for i in alerts:
             if i["plugin_output"] == newalert["plugin_output"] and \
@@ -107,15 +75,54 @@ class Create:
                 i["count"] += 1
                 duplicate = True
 
-        # Write alerts file
         if duplicate == False:
             alerts.append(newalert)
         with open(config.alertfile, "w+") as fh:
             fh.write(json.dumps(alerts))
 
+    def check_input(self, alert):
+        required = [ "plugin_output", "service", "status", "hostname" ]
+        for i in required:
+            if i not in alert.keys():
+                media = {
+                    "error": i + " is missing",
+                    "status": 400
+                }
+                return False
+
+        forbidden_chars = re.compile('[<>;]')
+        if forbidden_chars.search(alert["plugin_output"] + 
+        alert["service"] +
+        alert["hostname"] + 
+        alert["status"] ) != None:
+            return False
+
+        return True
+
+    def on_post(self, req, resp):
+        # Support a single object and a list of objects as well
+        if isinstance(req.media, list):
+            for i in req.media:
+                if self.check_input(i) == False:
+                    media = {
+                        "error": "Bad request",
+                        "status": 400
+                    }
+                    resp.status = falcon.HTTP_400
+                    resp.media = media
+                self.add_alert(i)
+        else:
+            if self.check_input(req.media) == False:
+                    media = {
+                        "error": "Bad request",
+                        "status": 400
+                    }
+                    resp.status = falcon.HTTP_400
+                    resp.media = media
+            self.add_alert(req.media)        
+
 class Ack:
     def on_post(self, req, resp, alert_id):
-        # Check request
         required = ["user_ad", "user_ip"]
         for i in required:
             if i not in req.media.keys():
@@ -127,28 +134,23 @@ class Ack:
                 resp.media = media
                 return
 
-        # Load existing alerts
         with open(config.alertfile) as fh:
             alerts = json.loads(fh.read())
 
-        # Delete alert with matching ID
         deleted_alert = {}
         for i in alerts:
             if i["id"] == alert_id:
                 deleted_alert = i
                 alerts.remove(i)
 
-        # Write alerts file
         with open(config.alertfile, "w") as fh:
             fh.write(json.dumps(alerts))
 
-        # Log ack
         with open(config.acklogfile, "a+") as fh:
             ack = req.media
             timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
             fh.write("[{}] Alert {} acknowledged by {} from {}. Data: hostname={} service={} plugin_output={}\n".format(timestamp, alert_id, ack["user_ad"], ack["user_ip"], deleted_alert["hostname"], deleted_alert["service"], deleted_alert["plugin_output"]))
 
-        # Set resp
         resp.media = {"success": "true"}
         resp.status = falcon.HTTP_200
         return
